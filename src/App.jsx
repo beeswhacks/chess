e0ef4afcec39a8bd76a08fd48340788eafd089ae
Jack Preston <jack.preston@smg.team>
2023-10-08 11:13:55 +0100

    feat: add basic chessboard grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        Hello world
-      </div>
-    </>
-  )
+    const getSquareColour = (index) => {
+        const rowNumber = Math.floor(index / 8);
+        return index % 2 === rowNumber % 2 ? 'bg-slate-400' : 'bg-slate-500';
+    }
+
+    return (
+        <>
+            <div className="aspect-square grid grid-cols-8 gap-0">
+                {Array(64)
+                    .fill()
+                    .map((_, index) => {
+                        return (
+                            <div key={index} className={`${getSquareColour(index)}`}>
+                            </div>
+                        );
+                    })}
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
