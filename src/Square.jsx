82c4d30171f8d086104d4a8a02c50e6741828088
Jack Preston <jack.preston@smg.team>
2023-10-10 20:40:43 +0100

    feat: add chess piece SVGs

--- src/Square.jsx
+++ src/Square.jsx
@@ -1,10 +1,34 @@
+
+// Import piece SVGs
+import { b, k, n, p, q, r, B, K, N, P, Q, R } from './assets';
+
+const pieceMap = new Map([
+    ['p', p],
+    ['n', n],
+    ['b', b],
+    ['r', r],
+    ['q', q],
+    ['k', k],
+    ['P', P],
+    ['N', N],
+    ['B', B],
+    ['R', R],
+    ['Q', Q],
+    ['K', K],
+]);
+
+
 const Square = ({ isDarkSquare, position, piece }) => {
     const lightColour = 'bg-emerald-100';
     const darkColour = 'bg-blue-950';
     const colour = isDarkSquare ? darkColour : lightColour;
     const textColor = colour === darkColour ? 'text-white' : 'text-black';
-    console.log(position + ': ' + piece);
-    return <div className={`${colour} ${textColor} text-center align-middle aspect-square`}>{piece}</div>;
+
+    return (
+        <div className={`${colour} ${textColor} text-center align-middle aspect-square`}>
+            {piece && <img src={pieceMap.get(piece)} alt={`${piece} on ${position}`} />}
+        </div>
+    );
 };
 
 export default Square;
---
+++ src/assets/index.js
@@ -0,0 +1,29 @@
+import bB from './pieces/bB.svg';
+import bK from './pieces/bK.svg';
+import bN from './pieces/bN.svg';
+import bP from './pieces/bP.svg';
+import bQ from './pieces/bQ.svg';
+import bR from './pieces/bR.svg';
+import wb from './pieces/wB.svg';
+import wK from './pieces/wK.svg';
+import wN from './pieces/wN.svg';
+import wP from './pieces/wP.svg';
+import wQ from './pieces/wQ.svg';
+import wR from './pieces/wR.svg';
+
+export {
+    // Black pieces
+    bB as b,
+    bK as k,
+    bN as n,
+    bP as p,
+    bQ as q,
+    bR as r,
+    // White pieces
+    wb as B,
+    wK as K,
+    wN as N,
+    wP as P,
+    wQ as Q,
+    wR as R,
+};
