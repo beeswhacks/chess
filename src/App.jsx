3625916728d80f01b6bdbed978b5c1ecaba1eda1
Jack Preston <jack.preston@smg.team>
2023-10-11 08:49:51 +0100

    prettify code

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import Board from './Board';
 import './App.css';
 
 function App() {
-    const [count, setCount] = useState(0);
-
     const game = new Game();
 
     return (
@@ -13,12 +11,12 @@ function App() {
             <header className="text-3xl font-semibold text-center m-3 p-3">
                 <h1>Chess</h1>
             </header>
-            <main className='m-3 md:columns-2'>
+            <main className="m-3 md:columns-2">
                 <section>
                     <Board game={game} />
                 </section>
                 <section>
-                    <h2 className='text-xl text-center m-3 p-3'>Options</h2>
+                    <h2 className="text-xl text-center m-3 p-3">Options</h2>
                     This is where the options will go
                 </section>
             </main>
